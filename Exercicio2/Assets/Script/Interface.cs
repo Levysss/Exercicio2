933d10d668f5dfcc7b8377d965c0ade93e330f20
Matheus Levy <srlevy008@gmail.com>
2024-02-29 18:34:51 -0300

    atualização

--- Exercicio2/Assets/Script/Interface.cs
+++ Exercicio2/Assets/Script/Interface.cs
@@ -7,43 +7,52 @@ public enum Aliens
     QUATRO_BRAÇOS,
     FANTASMATICO,
     ULTRA_T,
-    MASSA_CINZENTA
+    MASSA_CINZENTA,
+    HUMANO
+    
 
 }
 
 public class Interface : MonoBehaviour
 {
-    
-    
+    public Input input;
+    public Aliens aliens;
     void Update()
     {
         AliemSelecionado();
     }
     private void AliemSelecionado()
     {
-        if(Input.GetKeyUp(KeyCode.W))
+        
+       if(Input.GetKeyUp(KeyCode.W))
         {
             //fantasmatico
+            aliens = Aliens.FANTASMATICO;
             Debug.Log("Fantasmatico");
         }
         else if(Input.GetKeyUp(KeyCode.S))
         {
             //Massa cinzenta
+            aliens = Aliens.MASSA_CINZENTA;
              Debug.Log("Massa Cinzenta");
         }
         else if(Input.GetKeyUp(KeyCode.A))
         {
             //Quatro braços
+            aliens = Aliens.QUATRO_BRAÇOS;
              Debug.Log("Quatro Braços");
         }
         else if(Input.GetKeyUp(KeyCode.D))
         {
             //ultra T
+            aliens = Aliens.ULTRA_T;
              Debug.Log("Ultra T");
         }
-        else
+        else if(Input.GetKeyUp(KeyCode.Space))
         {
-            Debug.Log("Nem um alien Selecionado");
+            //Reset
+            aliens = Aliens.HUMANO;
+             Debug.Log("Voltou a ser humano");
         }
     }
 }
